5e63bbc07d0872bbb5a267c55c93a3f3c74a84e6
Ron Webb <ron@ronella.xyz>
2018-09-30 19:50:34 +1000

    Dependencies are now in its own jar.

--- src/main/resources/sqlsrvnt.bat
+++ src/main/resources/sqlsrvnt.bat
@@ -1,2 +1,15 @@
 @echo off
-java -jar lib\sql-servant-1.0.0-SNAPSHOT.jar %*
+
+rem Requires Java 8
+
+set LIBS_DIR=.\libs
+set LIBS=%LIBS_DIR%\commons-cli-1.4.jar;%LIBS_DIR%\commons-dbcp2-2.0.jar;%LIBS_DIR%\commons-io-2.6.jar;%LIBS_DIR%\commons-lang3-3.8.jar;%LIBS_DIR%\commons-logging-1.1.3.jar;%LIBS_DIR%\commons-pool2-2.2.jar;%LIBS_DIR%\groovy-all-2.3.11.jar;%LIBS_DIR%\gson-2.8.5.jar;%LIBS_DIR%\log4j-1.2.17.jar
+
+rem Add jdbc drivers here
+set DRVS_DIR=.\drivers
+set DRVS=%DRVS_DIR%\h2-1.4.197.jar
+
+set MAIN_JAR=%LIBS_DIR%\sql-servant-1.0.0-SNAPSHOT.jar
+set CLASSPATH=.;%LIBS%;%MAIN_JAR%;%DRVS%
+
+java -cp %CLASSPATH% sqlsrvnt %*
